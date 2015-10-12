3a72063cb29aa8f0faf0584f9e4821ad75b0422f
mandlebrot <lauren@robotlauren.com>
2015-10-12 17:05:59 -0600

    try to fix issue 4
    Code, minus auth tokens

--- Issue4.py
+++ Issue4.py
@@ -1,15 +1,15 @@
 from pipedrive import Pipedrive
 from twilio.rest import TwilioRestClient
 
-pd = Pipedrive('dc8fe731bb412373b21482cdb0a8a73c265afb18')      # PD login info
+pd = Pipedrive('Pipedrive_login')      # PD login info
 
 # Twilio login info
-account = "ACfeab90b395875933e09b902c3886c426"
-token = "5a4c2ac221f703dd091f9c9c92f54c55"
+account = "AC..."
+token = "Twilio_token"
 client = TwilioRestClient(account, token)
 
 call = client.calls.create(             # create new call
-    to="+13038065253",
+    to="any_number",
     from_="+15005550006",
     url="http://twimlets.com/holdmusic?Bucket=com.twilio.music.ambient")
 
@@ -20,7 +20,6 @@ new = pd.activities.post({       # create corresponding activity
     "subject": "click_to_call",
     "type": "call",
     "duration": call.duration,
-    "note": "test1",
     "deal_id": "693"
     })
 
